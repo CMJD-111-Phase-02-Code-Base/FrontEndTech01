bd51c66c5742c64f226b05dc3f86af4c159102f0
ThanuraSilva <thanurasilva1989@gmail.com>
2025-08-10 17:02:00 +0530

    Collect for data as JSON format

--- project/app.js
+++ project/app.js
@@ -4,3 +4,18 @@ const registerBtnClick = ()=>{
     window.location.href = "form.html"
 
 }
+//handle for data
+document.addEventListener("DOMContentLoaded",function(){
+    // load the form
+    const form = document.getElementById("data-form");
+    // get submit event 
+    form.addEventListener("submit",function(e){
+        e.preventDefault();
+        const formData = new FormData(form)
+        // add  data to an object
+        const formObj = Object.fromEntries(formData.entries())
+        console.log(formObj);
+        const formDataJSON = JSON.stringify(formObj)
+        console.log(formDataJSON)
+    });
+});
