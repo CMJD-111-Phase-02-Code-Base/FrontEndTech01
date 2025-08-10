0da58a6371fdf4fbab41c0344d572d69328f3411
ThanuraSilva <thanurasilva1989@gmail.com>
2025-08-10 16:32:24 +0530

    Handle click event

---
+++ concepts/basicsOfJS/app.js
@@ -0,0 +1,32 @@
+// console.log("Hello JS")
+
+var name = "Kamal";
+// let city = "Kalutara";
+
+// const age = 30
+// console.log("name:",name);
+// console.log("city:",city);
+// console.log("age",age);
+
+// let marks = 5;
+
+// if(marks >= 75){
+//     console.log("Super Pass")
+// }else if(marks > 50){
+//     console.log("Pass")
+// }else{
+//     console.log("Failed")
+// }
+
+function calc(num){
+   console.log(num * 10)
+}
+//calc(20)
+
+const calcNew = (num)=> console.log(num * 10)
+
+//calcNew(50)
+
+let myArray = ["Car","Van", true, 75];
+myArray.push(5000)
+console.log(myArray)
---
+++ project/app.js
@@ -0,0 +1,6 @@
+// Load form.html in the same window
+const registerBtnClick = ()=>{
+    // laod the page
+    window.location.href = "form.html"
+
+}
